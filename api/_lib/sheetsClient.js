395d71fcb7bd3083afe622270ee9efeec9b44bf5
Myletuyen <myletuyen1207@gmail.com>
2026-07-16 12:24:46 +0700

    Create sheetsClient.js

---
+++ api/_lib/sheetsClient.js
@@ -0,0 +1,10 @@
+async function fetchPublicCsv(spreadsheetId, sheetName) {
+    const url = `https://docs.google.com/spreadsheets/d/${spreadsheetId}/gviz/tq?tqx=out:csv&sheet=${encodeURIComponent(sheetName)}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Google Sheets returned HTTP ${response.status} for sheet "${sheetName}" (is it shared as "Anyone with the link can view"?)`);
+    }
+    return response.text();
+}
+
+module.exports = { fetchPublicCsv };
